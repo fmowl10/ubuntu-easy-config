92285415eda1f3347eca0c6be1e10837f7599944
minwook-shin <smw0106@naver.com>
2016-02-21 14:43:19 +0900

    remove korean files

--- ubuntu-easy-config.sh
+++ ubuntu-easy-config.sh
@@ -9,24 +9,24 @@
 
 
 
-VERSION=0.1.1 #버전
-argc=$#       #인자들
+VERSION=0.1.1 #version
+argc=$#
 argv0=$0
 argv1=$1
 
 
-#인자 처리
+#argcv process
 argcv()
 {
     if [ $argc -gt 0 ];
     then
         if [ "$argv1" == "-h" -o "$argv1" == "--help" ];
         then
-            echo -e "사용법: ./ubuntu-easy-config.sh [옵션]"
-            echo -e "옵션:"
-            echo -e "\t-h, --help\t도움말을 보여줍니다."
-            echo -e "\t-v, --version\t버전 정보를 보여줍니다."
-            echo -e "\t-s, --setting\t실행전 세팅을 합니다."
+            echo -e "How to use: ./ubuntu-easy-config.sh [option]"
+            echo -e "option:"
+            echo -e "\t-h, --help\t Show Help."
+            echo -e "\t-v, --version\t Show version information."
+            echo -e "\t-s, --setting\t Before running."
         fi
         if [ "$argv1" == "-v" -o "$argv1" == "--version" ];
         then
@@ -41,22 +41,18 @@ argcv()
     fi
 }
 
-#언어 파일 설정
+#Setting the language file
 setting(){
 	cd lang/
 	echo "script start"
-	msgfmt -o main_ko.mo main_ko.po
-	echo "main/messages_ko.mo created !"
 	msgfmt -o main_en.mo main_en.po
 	echo "main/messages_en.mo created !"
-	msgfmt -o desktop_ko.mo desktop_ko.po
-	echo "desktop/messages_ko.mo created !"
 	msgfmt -o desktop_en.mo desktop_en.po
 	echo "desktop/messages_en.mo created !"
 	exit 0
 }
 
-#앱실행
+#Launch app
 runnigapp(){
 	cd pages/
 	echo "run ubuntu-easy-config."
@@ -64,15 +60,15 @@ runnigapp(){
 	# echo "You can exit this terminal."
 }
 
-#권한체크
+#Check permissions
 checkingroot(){
 	if [ $(id -u) -ne 0 ]; then
-	        echo 이 스크립트는 루트 권한을 필요로 합니다. sudo ./ubuntu-easy-config.sh 로 실행시켜주십시오.
+	        echo This script requires root.$sudo ./ubuntu-easy-config.sh
 	        exit 0
         fi
 }
 
-#실행문
+#running core
 argcv
 checkingroot
 runnigapp
