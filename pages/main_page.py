fe6c34aa0dc49597414eeec4e1a8f2df8df9e061
minwook shin <smw0106@naver.com>
2016-02-22 11:49:20 +0900

    ADD license text

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,3 +1,9 @@
+# Part of ubuntu-easy-config https://github.com/minwook-shin/ubuntu-easy-config
+#
+# See LICENSE file for copyright and license details
+#
+# This program written for beginner for ubuntu and "ubuntu Setting" by minwook Shin and hedone21, fmowl10
+
 from gi.repository import Gtk
 from desktop_page import DesktopPage
 import common
