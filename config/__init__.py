3d152793bc6b569d1e93a66cd2899a7377e45721
minwook shin <smw0106@naver.com>
2016-02-22 11:51:01 +0900

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,9 @@
+# Part of ubuntu-easy-config https://github.com/minwook-shin/ubuntu-easy-config
+#
+# See LICENSE file for copyright and license details
+#
+# This program written for beginner for ubuntu and "ubuntu Setting" by minwook Shin and hedone21, fmowl10
+
 # You should put '/' at the end of path.
 # [PATH]
 global TEXT_PATH
